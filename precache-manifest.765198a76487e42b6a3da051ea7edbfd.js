0519366a25bf1eec6865de8e4029cc8bb3fef9ca
Wilson <wilsonj806@gmail.com>
2019-11-19 12:17:31 -0500

    Updates

---
+++ precache-manifest.765198a76487e42b6a3da051ea7edbfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5d068895630697367ba1820aebd149",
+    "url": "/ts-contributors-react-app/index.html"
+  },
+  {
+    "revision": "25ff0951b825411cc7d2",
+    "url": "/ts-contributors-react-app/static/css/main.ac8e676d.chunk.css"
+  },
+  {
+    "revision": "092c6ab27b6f37399d81",
+    "url": "/ts-contributors-react-app/static/js/2.048733b9.chunk.js"
+  },
+  {
+    "revision": "25ff0951b825411cc7d2",
+    "url": "/ts-contributors-react-app/static/js/main.d9349d73.chunk.js"
+  },
+  {
+    "revision": "f4b0fb42f36bffb7723e",
+    "url": "/ts-contributors-react-app/static/js/runtime-main.6cdda0b8.js"
+  }
+]);
